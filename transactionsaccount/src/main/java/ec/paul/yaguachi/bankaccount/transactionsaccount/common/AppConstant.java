567837bccf836dc7ffa0e80b082195a5c877607a
Paul Yaguachi <yaguachi.marcelo@gmail.com>
2024-12-09 08:53:19 -0500

    cambio url ws

--- transactionsaccount/src/main/java/ec/paul/yaguachi/bankaccount/transactionsaccount/common/AppConstant.java
+++ transactionsaccount/src/main/java/ec/paul/yaguachi/bankaccount/transactionsaccount/common/AppConstant.java
@@ -7,5 +7,6 @@ package ec.paul.yaguachi.bankaccount.transactionsaccount.common;
 public class AppConstant {
     public static final String DEPOSIT = "Deposito";
     public static final String WITHDRAWAL = "Retiro";
-    public static final String URL_CUSTOMER = "http://192.168.100.8:8082/customerperson/api/customer/" ;
+//    public static final String URL_CUSTOMER = "http://localhost:8082/customerperson/api/customer/" ;
+    public static final String URL_CUSTOMER = "http://customer-app:8082/customerperson/api/customer/" ;
 }
